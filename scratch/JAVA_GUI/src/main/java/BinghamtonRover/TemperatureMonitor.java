9088ae0084bb8aeb358e529fcc1f4b5b4522884f
edlforest <hzheng20@binghamton.edu>
2017-10-27 17:53:03 -0400

    Added some empty classes to start us off. Added some comments about what I think the class would monitor. Added dependency for json simple in pom.xml.

---
+++ scratch/JAVA_GUI/src/main/java/BinghamtonRover/TemperatureMonitor.java
@@ -0,0 +1,23 @@
+package BinghamtonRover;
+
+import java.util.Observable;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+
+/**
+ * The Temperature Monitor should monitor the temperature around the rover.
+ * The temperature will probably be measured in Kelvin.
+ */
+
+public class TemperatureMonitor extends InformationObserver{
+
+}
