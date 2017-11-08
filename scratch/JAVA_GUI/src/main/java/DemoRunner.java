adf02eb07f5b5ebd22a5046ba9337fa5151a03f0
Thomas Avery <tavery2@binghamton.edu>
2017-11-08 00:09:55 -0500

    Got GUI to show, created new Main file in /java/BinghamtonRover and called to it from DemoRunner, now need to test active change in JSON file update

--- scratch/JAVA_GUI/src/main/java/DemoRunner.java
+++ scratch/JAVA_GUI/src/main/java/DemoRunner.java
@@ -1,40 +1,10 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import BinghamtonRover.Main;
 
-public class DemoRunner extends Application
+
+public class DemoRunner
 {
-    @Override
-    public void start(Stage primaryStage)throws Exception{
-        /*
-        String lsFile = (args.length > 0) ? args[0] : "./src/main/files/python_output.log.json";
-
-        ArrayList<InformationObserver> laoObservers = new ArrayList<>();
-        laoObservers.add(new PressureMonitor());
-        laoObservers.add(new LocationMonitor());
-        laoObservers.add(new DistanceMonitor());
-        laoObservers.add(new BatteryMonitor());
-        laoObservers.add(new DirectionMonitor());
-        laoObservers.add(new TimeMonitor());
-        laoObservers.add(new TemperatureMonitor());
-        laoObservers.add(new CameraStatusMonitor());
-
-        FileUpdatingObservable lfuo = new FileUpdatingObservable(lsFile, laoObservers);
-        lfuo.startFileMonitoringThread();
-        */
-        //having trouble getting GUI to show
-
-        Parent root = FXMLLoader.load(getClass().getResource("UpdateGui.fxml"));
-        primaryStage.setTitle("GUI");
-        primaryStage.setScene(new Scene(root, 800, 800));
-        primaryStage.show();
-
-    }
-    public static void main(String[] args)
+    public static void main(String [] args)
     {
-        launch(args);
+        Main.run(args);
     }
-
 }
