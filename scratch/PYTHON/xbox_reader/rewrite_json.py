7390a84a068bb3699beb979da6d20d2ec4d7b7e5
13steinj <13steinj@users.noreply.github.com>
2017-11-15 00:15:17 -0500

    Remove JSON spec
    We are no longer using json

--- scratch/PYTHON/xbox_reader/rewrite_json.py
+++
@@ -1,5 +0,0 @@
-instructions = [] # limit-length container type
-
-
-def rewrite(json_file, instruction):
-    pass # do rewrite
--- scratch/PYTHON/xbox_reader/serialize_xbox.py
+++
@@ -1,9 +0,0 @@
-def serialize(raw_instruction):
-    # serialize the instruction as a python dict and return
-    pass
-
-
-def get_instruction():
-    # get the instruction
-    # return serialize(instruction)
-    pass
