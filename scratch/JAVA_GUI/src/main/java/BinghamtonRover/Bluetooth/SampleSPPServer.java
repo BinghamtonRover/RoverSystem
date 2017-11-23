f39aec77da6bc3629de87e62067d9a30fe4a2a1f
EDLForest <hzheng20@binghamton.edu>
2017-11-23 18:57:18 -0500

    Trying to add multi-client for server

--- scratch/JAVA_GUI/src/main/java/BinghamtonRover/Bluetooth/SampleSPPServer.java
+++ scratch/JAVA_GUI/src/main/java/BinghamtonRover/Bluetooth/SampleSPPServer.java
@@ -2,6 +2,7 @@ package BinghamtonRover.Bluetooth;
 
 import javafx.fxml.FXML;
 
+import javax.bluetooth.BluetoothStateException;
 import javax.bluetooth.LocalDevice;
 import javax.bluetooth.UUID;
 import javax.microedition.io.Connector;
@@ -14,36 +15,20 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import java.sql.Timestamp;
+import java.time.Instant;
+
 
 /**
  * A thread that opens an SPP connection, awaits client requests, accepts
  * incoming messages, and sends a response.
  */
-public class SampleSPPServer implements Runnable
-{
-    //Reference of the component of the GUI
-
-    //GUI Component not yet implemented
-    //Not sure if we want to use this class as the controller
-//    @FXML private Button coSendMessageButton;
-//    @FXML private TextArea coMessageBoard;
-//    @FXML private TextField coMessageInput;
+public class SampleSPPServer implements Runnable {
 
     private static final String gsSERVICE_NAME_SSP = "The SSP Server";
 
-
-    public void run()
-    {
-        try
-        {
-            StreamConnection connection;
-            DataOutputStream outputStream = null;
-            DataInputStream inputStream = null;
-            //Make this bufferReader final so it can be used in lambda
-            final BufferedReader loBufReader = new BufferedReader(new InputStreamReader(System.in));
-
-            try
-            {
+    public void run() {
+            try {
                 //Generate a new UUID and get the local device information
                 final UUID uuid = new UUID("1101", false);
                 final LocalDevice local = LocalDevice.getLocalDevice();
@@ -56,55 +41,19 @@ public class SampleSPPServer implements Runnable
 
                 // Open a connection and wait for client requests
                 // spawn a thread for each client connection in order to listen to multiple client simultaneously
-                final StreamConnectionNotifier service =
-                        (StreamConnectionNotifier) Connector.open("btspp://localhost:" + uuid + ";name=" + gsSERVICE_NAME_SSP);
-                connection = service.acceptAndOpen();
+                final StreamConnectionNotifier service = (StreamConnectionNotifier) Connector.open("btspp://localhost:" + uuid + ";name=" + gsSERVICE_NAME_SSP);
+                StreamConnection loConnection = service.acceptAndOpen();
                 updateStatus("[SERVER] SPP session created");
-
-                //Opening IOStream and system input reader
-                inputStream = connection.openDataInputStream();
-                outputStream = connection.openDataOutputStream();
-                while(true) {
-
-                    String lsMessage, lsReceivedMessage = null;
-//
-                    // Read a message
-                    //a buffer byte array used to temporarily hold the input, 1 kb should be enough for short text input
-                    final byte[] buffer = new byte[1024];
-                    //get the numbers of bytes of the input and then save the input to buffer
-                    final int readBytes = inputStream.read(buffer);
-                    //build a string of the receiving message from the buffer
-                    lsReceivedMessage= new String(buffer, 0, readBytes);
-                    //Print the received message
-                    updateStatus("[SERVER] Message received: " + lsReceivedMessage);
-
-                    // Send a message
-                    lsMessage = loBufReader.readLine();
-                    updateStatus("[SERVER] Sending message....");
-                    //Send out the message to the other end of the connection
-                    outputStream.write(lsMessage.getBytes());
-                    outputStream.flush();
-
-                }
             }
-
-            finally
+            catch (BluetoothStateException e)
             {
-                outputStream.close();
-                inputStream.close();
-                loBufReader.close();
-                updateStatus("[SERVER] SPP session closed");
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        }
-        catch (final IOException ioe)
-        {
-            //BluetoothJSR82Demo.errorDialog(ioe.toString());
-            ioe.printStackTrace();
-        }
     }
 
-    private void updateStatus(String asMessage)
-    {
+    private void updateStatus(String asMessage) {
         System.out.println(asMessage);
 //        coMessageBoard.appendText(asMessage);
     }
@@ -112,38 +61,88 @@ public class SampleSPPServer implements Runnable
     /**
      * Here is a thread class that will be created every time a connection is made
      * This thread is expected to handle the communication between the client and server
-     *
+     * <p>
      * when the server received a connection request, it will establish a connection and
      * instantiate a SeverClientConnection thread that listens to the message the client
      * sent and respond with, for now, a response string
      * Not yet implemented
      */
-    class ServerClientConnection extends Thread{
+    class ServerClientConnection extends Thread {
+
+        private StreamConnection coConnection;
+        private LocalDevice coDevice;
+
+        public ServerClientConnection(StreamConnection aoConnection, LocalDevice aoDevice)
+        {
+            coConnection = aoConnection;
+            coDevice = aoDevice;
+        }
+
+        public void run()
+        {
+
+            //Declare data IO stream
+            DataOutputStream outputStream = null;
+            DataInputStream inputStream = null;
+
+            try
+            {
+                outputStream = coConnection.openDataOutputStream();
+                inputStream = coConnection.openDataInputStream();
+
+                //Instantiate a timestamp to keep time of connection session time.
+                Timestamp loTimestamp = new Timestamp(System.currentTimeMillis());
+                updateStatus("Session started at: " + loTimestamp);
+
+
+
+                long lnSessionInactiveTime = System.currentTimeMillis() - loTimestamp.getTime();
+                while(lnSessionInactiveTime < 10000)
+                {
+                    String lsMessage =  "Server Says Hello";
+                    String lsReceivedMessage;
+//
+                    // Read a message
+                    //a buffer byte array used to temporarily hold the input, 1 kb should be enough for short text input
+                    //get the numbers of bytes of the input and then save the input to buffer
+                    //build a string of the receiving message from the buffer
+                    //then print the received message
+                    final byte[] buffer = new byte[1024];
+                    final int readBytes = inputStream.read(buffer);
+                    lsReceivedMessage = new String(buffer, 0, readBytes);
+                    if(!lsReceivedMessage.isEmpty())
+                    {
+                        updateStatus("[SERVER] Message received: " + lsReceivedMessage);
+                        loTimestamp = new Timestamp(System.currentTimeMillis());
+                    }
+                    // Send a message
+                    //Send out the message to the other end of the connection
+                    updateStatus("[SERVER] Sending message....");
+                    outputStream.write(lsMessage.getBytes());
+                    outputStream.flush();
+
+                    //Recalculate the session inactive time
+                    lnSessionInactiveTime = System.currentTimeMillis() - loTimestamp.getTime();
+                }
+                updateStatus("[SERVER] Session ended with " + coDevice.getFriendlyName());
+                outputStream.close();
+                inputStream.close();
+
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+
+        }
 
     }
-
 
 
     public static void main(String[] args) throws IOException {
 
-
-
-        //start the Server
         SampleSPPServer sampleSPPServer = new SampleSPPServer();
         sampleSPPServer.run();
-//
-//        //Launch the GUI
-//        launch(args);
+
     }
-
-    //not sure where to place the GUI controller
-//    @Override
-//    public void start(Stage aoPrimaryStage) throws Exception
-//    {
-//        Parent loRoot = FXMLLoader.load(getClass().getResource("/fxml/BluetoothGUI.fxml"));
-//        aoPrimaryStage.setTitle("BluetoothGUI");
-//        aoPrimaryStage.setScene(new Scene(loRoot));
-//        aoPrimaryStage.show();
-//    }
-
 }
