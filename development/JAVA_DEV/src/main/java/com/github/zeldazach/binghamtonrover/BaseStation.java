d6b9e9e600dea802d829025aad657d92f971c012
Layne Gustafson <0@laynegustafson.com>
2018-01-24 10:31:39 -0500

    Initial commit of base station program.

---
+++ development/JAVA_DEV/src/main/java/com/github/zeldazach/binghamtonrover/BaseStation.java
@@ -0,0 +1,12 @@
+package com.github.zeldazach.binghamtonrover;
+
+public class BaseStation {
+
+    /**
+     * The entry point for the base station control program.
+     * @param args Command-line arguments.
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ development/JAVA_DEV/src/main/java/com/github/zeldazach/binghamtonrover/controller/ControllerState.java
@@ -0,0 +1,4 @@
+package com.github.zeldazach.binghamtonrover.controller;
+
+public class ControllerState {
+}
