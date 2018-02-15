0ea0ade52d3fb1934756f2591def14288cda1f28
yuxuwu <ywu148@binghamton.edu>
2018-02-15 03:23:54 -0500

    First commit for ROS team, added sample publisher subscriber callback program (#50)

---
+++ scratch/CPP_ROVER/ros_workspace/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/IEEE_PI/ros_workspace/src
+# Build directory: /home/IEEE_PI/ros_workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(automatic_rover_boy)
---
+++ scratch/CPP_ROVER/ros_workspace/build/automatic_rover_boy/CMakeFiles/talker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/talker.dir/src/talker.cpp.o"
+  "/home/IEEE_PI/ros_workspace/devel/lib/automatic_rover_boy/talker.pdb"
+  "/home/IEEE_PI/ros_workspace/devel/lib/automatic_rover_boy/talker"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/talker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
