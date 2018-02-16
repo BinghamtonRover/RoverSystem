bf42d5ec8bbdeee70c00c9942a5a84a892b42f9e
13steinj <13steinj@users.noreply.github.com>
2018-02-16 05:10:32 -0500

    Boaty mc boat face

--- development/JAVA_DEV_BASE_STATION/src/main/java/com/github/zeldazach/binghamtonrover/networking/PacketCameraHandler.java
+++ development/JAVA_DEV_BASE_STATION/src/main/java/com/github/zeldazach/binghamtonrover/networking/PacketCameraHandler.java
@@ -8,82 +8,116 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.util.Arrays;
+
 
 class FrameBuffer {
-    private ByteBuffer[][] internalBuffer;
+    int timestamp, remaining_sections, buffer_size;
+    ByteBuffer[] internal_buffers;
 
-    private int[] timestamps;
-
-    private int size;
-
-    FrameBuffer(int size) {
-        this.size = size;
-        timestamps = new int[size];
-        internalBuffer = new ByteBuffer[5][0]; // internal arrays are changed anyway
+    FrameBuffer(int numBuffers) {
+        timestamp = remaining_sections = buffer_size = 0;
+        internal_buffers = new ByteBuffer[numBuffers];
     }
 
-    private void updateFrame(PacketCamera packet, int index) {
-        internalBuffer[index][packet.getSectionIndex()] = packet.getSectionData();
-        if (!Arrays.asList(internalBuffer[index]).contains(null)) {
-            // frame is complete
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            try {
-                for (ByteBuffer buff: internalBuffer[index]) {
-                    baos.write(buff.array());
-                }
-            } catch (IOException ex) {
-                System.err.println("Couldn't concat complete frame TIMESTAMP: " + timestamps[index]);
-                return;
-            }
-            Image image = new Image(new ByteArrayInputStream(baos.toByteArray()));
-            Platform.runLater(() -> {
-                if (DisplayApplication.INSTANCE != null) {
-                    DisplayApplication.INSTANCE.getCameraImageView().setImage(image);
-                }
-            });
+    void push() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            for (ByteBuffer buff : internal_buffers) baos.write(buff.array());
+        } catch (IOException ex) {
+            System.out.println("boaty mc boat face");
         }
+        Image image = new Image(new ByteArrayInputStream(baos.toByteArray()));
+        Platform.runLater(() -> {
+            if (DisplayApplication.INSTANCE != null) {
+                DisplayApplication.INSTANCE.getCameraImageView().setImage(image);
+            }
+        });
+    }
+}
+
+class FrameBufferContainer {
+    private int total_frames_received = 0;
+    private int total_frames_dropped = 0;
+    private int next_buffer;
+    private FrameBuffer[] buffers;
+
+    private int maxSize;
+
+    FrameBufferContainer(int maxSize) {
+        this.maxSize = maxSize;
+        next_buffer = 0;
+        buffers = new FrameBuffer[maxSize];
     }
 
-    private void shiftBuffer(PacketCamera packet) {
-        int[] tempTimestamps = new int[size];
-        ByteBuffer[][] tempInternalBuffer = new ByteBuffer[size][0];
-        System.arraycopy(timestamps, 1, tempTimestamps, 0, size - 1);
-        System.arraycopy(internalBuffer, 1, tempInternalBuffer, 0, size - 1);
-        timestamps[size - 1] = packet.getHeader().getTimestamp();
-        internalBuffer[size - 1] = new ByteBuffer[packet.getSectionCount()];
-        timestamps = tempTimestamps;
-        internalBuffer = tempInternalBuffer;
-    }
-
-    public void update(PacketCamera packet) {
-        boolean timestampWithin = false;
-        for (int i = 0; i < timestamps.length; i++) {
-            if (timestamps[i] == 0) {
-                timestamps[i] = packet.getHeader().getTimestamp();
-                internalBuffer[i] = new ByteBuffer[packet.getSectionCount()];
-                timestampWithin = true;
-                updateFrame(packet, i);
-            } else if (timestamps[i] == packet.getHeader().getTimestamp()) {
-                updateFrame(packet, i);
-                timestampWithin = true;
+    void update_buffer(PacketCamera packet) {
+        int timestamp = packet.getHeader().getTimestamp();
+        int section_count = packet.getSectionCount();
+        int section_id = packet.getSectionIndex();
+        ByteBuffer frame_data = packet.getSectionData();
+        int frame_data_size = packet.getSectionSize();
+        // Search for a buffer that already has that timestamp.
+        int found_buffer_idx = -1;
+        for (int i = 0; i < maxSize; i++) {
+            if (buffers[i].timestamp == timestamp) {
+                // Found it!
+                found_buffer_idx = i;
+                break;
             }
         }
-        if (!timestampWithin) {
-            shiftBuffer(packet);
-            updateFrame(packet, size - 1);
+
+        if (found_buffer_idx == -1) {
+            // We did not find a buffer... it is a new frame!
+            total_frames_received++;
+
+            FrameBuffer our_buffer = buffers[next_buffer] = new FrameBuffer(section_count);
+
+            if (our_buffer.timestamp != 0) {
+                // It is not a new buffer... it already has a frame in it.
+
+                if (our_buffer.remaining_sections == 0) {
+                    // We are ready to push to screen!
+
+                    our_buffer.push();
+                } else {
+                    System.out.println("> Dropped frame with timestamp " + our_buffer.timestamp + ", dropped perc " + total_frames_dropped / (double) total_frames_received);
+                    total_frames_dropped++;
+                }
+            }
+
+            our_buffer.timestamp = timestamp;
+            our_buffer.remaining_sections = section_count - 1;
+            our_buffer.internal_buffers[section_id] = frame_data;
+            our_buffer.buffer_size = frame_data_size;
+
+            if (our_buffer.remaining_sections == 0) {
+                our_buffer.push();
+            }
+
+            // Reset our next_buffer.
+            next_buffer = (next_buffer + 1) % maxSize;
+        } else {
+            // We found our buffer, let's update it!
+
+            FrameBuffer our_buffer = buffers[found_buffer_idx];
+
+            our_buffer.remaining_sections--;
+            our_buffer.internal_buffers[section_id] = frame_data;
+            our_buffer.buffer_size += frame_data_size;
+
+            if (our_buffer.remaining_sections == 0) {
+                our_buffer.push();
+            }
         }
     }
 }
 
-
 public class PacketCameraHandler implements PacketHandler {
-    private FrameBuffer frameBuffer = new FrameBuffer(5);
+    private FrameBufferContainer frameBufferContainer = new FrameBufferContainer(5);
 
     @Override
     public void handle(Packet packet) {
         try {
-            frameBuffer.update((PacketCamera) packet);
+            frameBufferContainer.update_buffer((PacketCamera) packet);
         } catch (IllegalArgumentException e) {
             System.out.println("Unable to add section to potential frame " + e.getMessage());
         }
