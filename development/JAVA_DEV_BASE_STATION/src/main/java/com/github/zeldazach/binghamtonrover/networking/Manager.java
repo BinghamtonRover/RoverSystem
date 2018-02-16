524c49c86c43c1219b745c06b28c117d088d3ffa
Layne Gustafson <0@laynegustafson.com>
2018-02-16 00:06:38 -0500

    Increment BS-side network protocol version

--- development/JAVA_DEV_BASE_STATION/src/main/java/com/github/zeldazach/binghamtonrover/networking/Manager.java
+++ development/JAVA_DEV_BASE_STATION/src/main/java/com/github/zeldazach/binghamtonrover/networking/Manager.java
@@ -34,7 +34,7 @@ public class Manager
         }
     }
 
-    private static final int CURRENT_VERSION = 4; // The current supported version.
+    private static final int CURRENT_VERSION = 5; // The current supported version.
     private InetAddress address;
     private int port;
     private int resendCount;
