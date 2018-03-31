ff34dc231d838f76c580e3841ca1ca3731792cb3
yuxuwu <ywu148@binghamton.edu>
2018-03-31 16:50:02 -0400

    Reworked BallTracking module
    Now uses HSV(Hue Saturation Value) range to detect tennis ball
    Using Contour detection to find Tennis Ball instead of HoughCircles
    
    -- Need to work on:
    * Depth measurements calculated from radius on contour
    * Integration of multiple webcams (stetch goal for now?)

--- scratch/CPP_ROVER/ros_workspace/src/BallTracking/src/main.cpp
+++ scratch/CPP_ROVER/ros_workspace/src/BallTracking/src/main.cpp
@@ -10,101 +10,121 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include "Camera.h"
 
+using namespace std;
+using namespace cv;
+
 double WIDTH = 6.86; //Centimeters
 double FOCAL = 700; //Dummy for now
 double RESOLUTION = 1920 * 1080;
 
+int thresh = 100;
+
 int main(int argc, char ** argv) {
-	std::vector<int> height, width, step;
-	std::vector<cv::Size> sizes;
+    std::vector<int> height, width, step;
+    std::vector<cv::Size> sizes;
 
-	int k = 1;
-	std::vector<cv::VideoCapture> captures;
-	captures.push_back(cv::VideoCapture(0));
+    int k = 1;
+    std::vector<cv::VideoCapture> captures;
+    captures.push_back(cv::VideoCapture(0));
 
-	while (captures[k - 1].isOpened()) {
-		cv::VideoCapture cap = cv::VideoCapture(k);
-		if (cap.isOpened()) {
-			captures.push_back(cap);
-		}
-		else {
-			break;
-		}
-		k++;
-	}
+    captures[0].set(CV_CAP_PROP_FRAME_WIDTH , 352);
+    captures[0].set(CV_CAP_PROP_FRAME_HEIGHT , 288);
+    while (captures[k - 1].isOpened()) {
+        cv::VideoCapture cap = cv::VideoCapture(k);
+        if (cap.isOpened()) {
+            captures[k].set(CV_CAP_PROP_FRAME_WIDTH , 352);
+            captures[k].set(CV_CAP_PROP_FRAME_HEIGHT , 288);
+            captures.push_back(cap);
+        }
+        else {
+            break;
+        }
+        k++;
+    }
 
-	int numVideoFeeds = captures.size();
-	std::vector<cv::Mat> frames;
-	std::vector<cv::Mat> thresholded_gray;
+    cvNamedWindow("Mask", CV_WINDOW_AUTOSIZE);
 
-	for (int i = 0; i < captures.size(); i++) {
-		std::string temporary = "Camera: " + std::to_string(i);
-		const char* windowName = temporary.c_str();
-		cv::Mat frame;
-		captures[i].read(frame);
-		frames.push_back(frame);
-		cvNamedWindow(windowName, CV_WINDOW_AUTOSIZE);
-		height.push_back(frame.rows);
-		width.push_back(frame.cols);
-		step.push_back(frame.step);
-		sizes.push_back(cv::Size(width[i], height[i]));
-		cv::Mat temp(sizes[i], IPL_DEPTH_8U, 1);
-		thresholded_gray.push_back(temp);
-	}
+    int numVideoFeeds = captures.size();
+    std::vector<cv::Mat> frames;
+    std::vector<cv::Mat> thresholded_gray;
 
-	while (1)
-	{
-		// Get one frame
-		for (int i = 0; i < numVideoFeeds; i++) {
-			bool val = captures[i].read(frames[i]);
-			if (!val) {
-				fprintf(stderr, "ERROR: cannot capture image on camera\n");
-				getchar();
-				break;
-			}
-			//Smooth image to reduce noise
-			GaussianBlur(frames[i], thresholded_gray[i], cv::Size(9, 9), 3, 3, cv::BORDER_DEFAULT);
+    for (int i = 0; i < captures.size(); i++) {
+        std::string temporary = "Camera: " + std::to_string(i);
+        const char* windowName = temporary.c_str();
+        cv::Mat frame;
+        captures[i].read(frame);
+        frames.push_back(frame);
+        cvNamedWindow(windowName, CV_WINDOW_AUTOSIZE);
+        height.push_back(frame.rows);
+        width.push_back(frame.cols);
+        step.push_back(frame.step);
+        sizes.push_back(cv::Size(width[i], height[i]));
 
-			//Convert to grayscale to do houghCircle transformation
-			cvtColor(thresholded_gray[i], thresholded_gray[i], CV_BGR2GRAY);
-		}
+        cv::Mat temp(sizes[i], IPL_DEPTH_8U, 1);
+        thresholded_gray.push_back(temp);
+    }
 
-		for (int i = 0; i < numVideoFeeds; i++) {
-			std::vector<cv::Vec3f> circles;
-			cv::HoughCircles(thresholded_gray[i], circles, CV_HOUGH_GRADIENT, 2,
-				thresholded_gray[i].rows / 4, 100, 100, 20, 200);
+    while (1) {
+        // Get one frame
+        for (int i = 0; i < numVideoFeeds; i++) {
+            bool val = captures[i].read(frames[i]);
+            if (!val) {
+                fprintf(stderr, "ERROR: cannot capture image on camera\n");
+                getchar();
+                break;
+            }
+        }
 
-			for (int j = 0; j < circles.size(); j++)
-			{
-				cv::Point center(cvRound(circles[j][0]), cvRound(circles[j][1]));
-				int radius = cvRound(circles[j][2]);
+        for (int i = 0; i < numVideoFeeds; i++) {
+            /// HSV mask and contours container
+            cv::Mat mask;
+            std::vector< std::vector< cv::Point>> contours;
 
-				//TODO Need to initialize focal length
-				int pixelWidth = radius * 2;
-				int distance = (WIDTH * FOCAL) / pixelWidth;
-				printf("Distance (centimeters): %d\n", distance);
+            /*---Pre-processing Image Transformations---*/
+            /// Blur to allow for faster processing ? Not sure if neccesary ! Remove grayscale processing
+            GaussianBlur(frames[i], thresholded_gray[i], cv::Size(9, 9), 3, 3, cv::BORDER_DEFAULT);
+            /// Change to HSV
+            cvtColor(frames[i], mask , CV_BGR2HSV);
+            /// Apply color range for HSV
+            cv::inRange(mask, cv::Scalar(29, 51, 6), cv::Scalar(64, 255, 255), mask);
+            /// Erode and dilate to remove unncessary shapes
+            cv::erode(mask, mask, Mat(), Point(-1, -1), 2, BORDER_CONSTANT, morphologyDefaultBorderValue());
+            cv::dilate(mask, mask, Mat(), Point(-1, -1), 2, BORDER_CONSTANT, morphologyDefaultBorderValue());
 
-				//TODO ANGLE CALCULATIONS
+            /*---Find contours in mask---*/
+            cv::findContours(mask, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+            //If contours found, measure and draw them
+            if(contours.size() > 0){
+                /// Sort to find largest contour
+                sort(contours.begin(), contours.end(), [&contours](vector<Point> lhs, vector<Point> rhs){
+                    return contourArea(lhs) > contourArea(rhs);
+                });
 
-				//Draw circle in the center of the ball indicating origin
-				circle(frames[i], center, 3, cv::Scalar(0, 255, 0), -1, 8, 0);
+                /// Bounding circle for largest contour
+                Point2f contour_point;
+                float contour_radius;
+                minEnclosingCircle( (Mat)contours[0], contour_point, contour_radius);
+                if(contour_radius > 15){
+                    //Draw biggest bounding circle on original image
+                    drawContours(frames[i], contours, 0, Scalar(255, 0, 0), 1, 8, vector<Vec4i>(), 0, Point());
+                    circle(frames[i], contour_point, (int)contour_radius, Scalar(0, 0, 255), 2, 8 ,0);
+                }
+            }
 
-				//Draw circle around ball
-				circle(frames[i], center, radius, cv::Scalar(0, 0, 255), 3, 8, 0);
-			}
+            std::string temporary = "Camera: " + std::to_string(i);
+            const char* windowName = temporary.c_str();
 
-			std::string temporary = "Camera: " + std::to_string(i);
-			const char* windowName = temporary.c_str();
+            cv::imshow(windowName, frames[i]);
+            cv::imshow("Mask", mask);
 
-			cv::imshow(windowName, frames[i]);
-		}
+        }
 
-		//If ESC key pressed, close video feed
-		if ((cvWaitKey(10) & 255) == 27) break;
-	}
+        //If ESC key pressed, close video feed
+        if ((cvWaitKey(10) & 255) == 27) break;
+    }
 
-	// Release the capture device
-	for (int i = 0; i < numVideoFeeds; i++) captures[i].release();
+    // Release the capture device
+    for (int i = 0; i < numVideoFeeds; i++) captures[i].release();
 
-	return 0;
+    return 0;
 }
