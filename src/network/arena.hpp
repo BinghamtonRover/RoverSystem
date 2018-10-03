4bcdea23dd9ccffffeb06b4406fb675031d0680e
Layne Gustafson <0@laynegustafson.com>
2018-10-03 16:21:43 -0400

    Modify the arena allocator to use the standard library stack.

--- src/network/arena.hpp
+++ src/network/arena.hpp
@@ -5,66 +5,12 @@
 #include <cstddef>
 #include <cstdint>
 
-/*
-    This file defines a Stack and an Arena type which are used exclusively within the network library.
-    These are all in a header file simply because they rely exclusively on templates, so they have to be.
-*/
+#include <stack>
 
 /*
-    This is your standard stack.
-    It uses methods / constructor syntax due to the close relationship between data and operations.
+    This file defines an Arena allocator which is used exclusively within the network library.
 */
-template <typename T>
-class Stack {
-private:
 
-    int len;
-    int cap;
-    T* items = nullptr;
-
-public:
-
-    Stack(int _cap) {
-        cap = _cap;
-        len = 0;
-
-        items = new T[cap];
-    }
-
-    ~Stack() {
-        if (items) delete[] items;
-    }
-
-    // Puts an element on the top of the stack.
-    void push(T t) {
-        if (len == cap) {
-            // We have filled our remaining space.
-            // Increase capacity by factor of 2, reallocate, and copy the data.
-
-            cap = cap * 2;
-            T* new_items = new T[cap];
-            memcpy(new_items, items, len * sizeof(T));
-            delete[] items;
-            items = new_items;
-        }
-
-        // Append the item.
-        items[len++] = t;
-    }
-
-    // Takes the top element off the stack.
-    // Returns false if the stack was empty.
-    bool pop(T& t) {
-        if (len > 0) {
-            t = items[len - 1];
-            len--;
-            return true;
-        }
-
-        return false;
-    }
-
-};
 
 // A way to store the index of each allocated T.
 template <typename T>
@@ -84,11 +30,11 @@ private:
     int segment_size;
     TContainer<T>** segments;
 
-    Stack<int> indices;
+	std::stack<int> indices;
 
 public:
 
-    Arena(int _segment_size) : indices(_segment_size) {
+    Arena(int _segment_size) {
         segment_size = _segment_size;
 
         // Create the segments array.
@@ -118,7 +64,7 @@ public:
     T* alloc() {
         int index;
 
-        if (!indices.pop(index)) {
+        if (indices.empty()) {
             // If there are no free indices, then we must create a new segment.
             TContainer<T>** new_segments = new TContainer<T>*[num_segments + 1];
             memcpy(new_segments, segments, num_segments*sizeof(TContainer<T>*));
@@ -136,9 +82,10 @@ public:
                 // We subtract that big thing off because its our initial i offset.
                 segments[num_segments - 1][i - (num_segments - 1)*segment_size].index = i;
             }
-
-            indices.pop(index);
         }
+
+		index = indices.top();
+		indices.pop();
 
         // When we get an index, we need to find the segment to which it belongs.
         // The remainder is the offset within that segment.
