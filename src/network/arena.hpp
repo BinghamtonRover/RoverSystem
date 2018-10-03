2cd30c50d29bf36f2658d270f7bfd256c68ad703
Layne Gustafson <0@laynegustafson.com>
2018-10-03 16:28:46 -0400

    Modify the arena allocator to use the standard library vector.

--- src/network/arena.hpp
+++ src/network/arena.hpp
@@ -26,9 +26,9 @@ template <typename T>
 class Arena {
 private:
 
-    int num_segments;
     int segment_size;
-    TContainer<T>** segments;
+	// `segments` is a vector of pointers to segments of memory used for allocation.
+	std::vector<TContainer<T>*> segments;
 
 	std::stack<int> indices;
 
@@ -37,11 +37,8 @@ public:
     Arena(int _segment_size) {
         segment_size = _segment_size;
 
-        // Create the segments array.
-        segments = new TContainer<T>*[1];
-
         // Create the first segment.
-        segments[0] = new TContainer<T>[segment_size];
+		segments.push_back(new TContainer<T>[segment_size]);
 
         // Update the indices of the first segment
         // and push them so we know they are available.
@@ -53,11 +50,9 @@ public:
 
     // Destroys the segments.
     ~Arena() {
-        for (int i = 0; i < num_segments; i++) {
-            if (segments[i]) delete[] segments[i];
+        for (size_t i = 0; i < segments.size(); i++) {
+            delete[] segments[i];
         }
-
-        delete[] segments;
     }
 
     // Grabs an available T.
@@ -66,21 +61,15 @@ public:
 
         if (indices.empty()) {
             // If there are no free indices, then we must create a new segment.
-            TContainer<T>** new_segments = new TContainer<T>*[num_segments + 1];
-            memcpy(new_segments, segments, num_segments*sizeof(TContainer<T>*));
-            delete[] segments;
-            segments = new_segments;
-            num_segments++;
-            segments[num_segments - 1] = new TContainer<T>[segment_size];
+			segments.push_back(new TContainer<T>[segment_size]);
 
             // Set the indices of the new segment.
             // Also push those indices to the stack.
-            // Since num_segments was already incremented, we subtrack 1 here. Not the best, not the worst.
-            for (int i = (num_segments - 1)*segment_size; i < num_segments*segment_size; i++) {
-                indices.push(i);
-                // It might be cleaner just to have i go from 0 to segment_size.
-                // We subtract that big thing off because its our initial i offset.
-                segments[num_segments - 1][i - (num_segments - 1)*segment_size].index = i;
+            for (size_t i = 0; i < segments.size(); i++) {
+				// Convert to a "global" index (from the first segment).
+                indices.push((segments.size() - 1) * segment_size);
+
+                segments[segments.size() - 1][i].index = i;
             }
         }
 
