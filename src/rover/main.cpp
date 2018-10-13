cc5f37588b962606e6fa4cc7d5caa9d9825e999f
yuxuwu <ywu148@binghamton.edu>
2018-10-13 18:30:33 -0400

    Included network package into rover/main.cpp

--- src/rover/main.cpp
+++ src/rover/main.cpp
@@ -1,33 +1,41 @@
 #include "camera.hpp"
 #include <stddef.h>
 #include <iostream>
+#include "../network/network.hpp"
 
 int main() {
+	/// Initialization of Rover objects
+	// OPTIMIZE: Consider making a rover class with an iteratable list of cameras
 	// Open all cameras
+	camera::CaptureSession session0;
 	camera::CaptureSession session1;
 	camera::CaptureSession session2;
 	camera::CaptureSession session3;
-	camera::CaptureSession session4;
 
-	std::cout << "Hello Wowld:" << std::endl;
+	camera::Error err0 = camera::open(&session0, "/dev/video0", 1280, 720);
+	std::cout << "Camera 0: " << camera::get_error_string(err0) << std::endl;
+	camera::Error err1 = camera::open(&session1, "/dev/video1", 1280, 720);
+	std::cout << "Camera 1: " << camera::get_error_string(err0) << std::endl;
+	camera::Error err2 = camera::open(&session2, "/dev/video2", 1280, 720);
+	std::cout << "Camera 2: " << camera::get_error_string(err0) << std::endl;
+	camera::Error err3 = camera::open(&session3, "/dev/video3", 1280, 720);
+	std::cout << "Camera 3: " << camera::get_error_string(err0) << std::endl;
 
-	/* TODO:Consider making each instance its own object */
-	camera::Error err1 = camera::open(&session1, "/dev/video0", 1280, 720);
-	//std::cout << "1 " << camera::get_error_string(err1) << std::endl;
+	err0 = camera::start(&session0);
+	std::cout << "Camera 0: " << camera::get_error_string(err0) << std::endl;
 	err1 = camera::start(&session1);
-	//std::cout << camera::get_error_string(err1) << std::endl;
-
-	camera::Error err2 = camera::open(&session2, "/dev/video0", 1280, 720);
+	std::cout << "Camera 1: " << camera::get_error_string(err1) << std::endl;
 	err2 = camera::start(&session2);
-	//std::cout << camera::get_error_string(err2) << std::endl;
-
-	camera::Error err3 = camera::open(&session3, "/dev/video0", 1280, 720);
+	std::cout << "Camera 2: " << camera::get_error_string(err2) << std::endl;
 	err3 = camera::start(&session3);
-	//std::cout << camera::get_error_string(err3) << std::endl;
+	std::cout << "Camera 3: " << camera::get_error_string(err3) << std::endl;
 
-	camera::Error err4 = camera::open(&session4, "/dev/video0", 1280, 720);
-	err4 = camera::start(&session4);
-	//std::cout << camera::get_error_string(err4) << std::endl;
+	// Open UDP connection
+	network::Connection conn;
+	network::Error net_err = network::connect(&conn, "192.168.1.1", 45545, 45545);
+	if((net_err != network::Error::OK)) {
+		std::cerr << "[!]Failed to connect to rover!" << std::endl;
+	}
 
 	while(true) {
 		//Grab a frame
@@ -40,11 +48,11 @@ int main() {
 		//std::cout << "2" << camera::get_error_string(err1) << std::endl;
 
 	}
-
-	// Close the session at the end
+	/// Destruct Rover objects
+	// Close the camera session
+	camera::close(&session0);
 	camera::close(&session1);
 	camera::close(&session2);
 	camera::close(&session3);
-	camera::close(&session4);
 	return 0;
 }
