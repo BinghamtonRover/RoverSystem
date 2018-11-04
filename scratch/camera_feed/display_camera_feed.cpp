3425dd5820b35d1db140b510cde65ddc0ed5b2d3
HenryBurns <hhburnsboy@gmail.com>
2018-11-04 11:07:05 -0500

    Deleted hpp and replaced #define with const ints

--- scratch/camera_feed/display_camera_feed.cpp
+++ scratch/camera_feed/display_camera_feed.cpp
@@ -1,22 +1,37 @@
 #include "../../src/network/network.hpp"
-#include "display_camera_feed.hpp"
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
 #include <turbojpeg.h>
 #include "SDL2/SDL.h"
 //#include <inttypes.h> to print uints
+// If you are trying to print a uintxx_t there are 2 methods I have found.
+/*
+    std::cout << unsigned(frame.section_count) << std::endl;
+ */
+/*
+    printf("%" PRIu16 "\n", uint you are trying to print);
+ */
+struct BufferItem{
+    uint16_t frame_index;
+    uint16_t data_size;
+    uint8_t sections_remaining;
+    unsigned char* data_sections;
+};
 
-#define CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE 65000
-#define CAMERA_FRAME_BUFFER_SIZE 6220800
-#define CAMERA_FRAME_BUFFER_COUNT 5
-#define WINDOW_X_LENGTH 1920 //window resolution. Standard is 1920
-#define WINDOW_Y_LENGTH 1080 //my window resolution. Standard is 1080
-
-const long unsigned int _jpegSize = WINDOW_X_LENGTH * WINDOW_Y_LENGTH * 3;
+const int CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE  = 65000;
+const int CAMERA_FRAME_BUFFER_SIZE = 6220800;
+const int CAMERA_FRAME_BUFFER_COUNT = 5;
+/*
+ This will change the size of the window rendered.
+ Standard Resolution is 1920 by 1080
+ */
+const int WINDOW_X_LENGTH = 1920;
+const int WINDOW_Y_LENGTH = 1080;
+const long unsigned int JPEG_SIZE = WINDOW_X_LENGTH * WINDOW_Y_LENGTH * 3;
+tjhandle _jpegDecompressor = tjInitDecompress();
 
 void decompress(unsigned char* buffer, unsigned char* compressed_image, int pitch){
-    tjhandle _jpegDecompressor = tjInitDecompress();
     if(_jpegDecompressor == NULL)
         std::cout << "decompressor was not made" << std::endl;
     /*
@@ -25,57 +40,76 @@ void decompress(unsigned char* buffer, unsigned char* compressed_image, int pitc
         the height, the pixel type (We have RGB here each contained in 1 byte),
         and the flags. Basically we say decompress it quickly.
     */
-    //std::cout << ". JPG size: " << _jpegSize <<  ". Pitch " << pitch << std::endl;
-    if (tjDecompress2(_jpegDecompressor, compressed_image, _jpegSize, buffer, WINDOW_X_LENGTH, pitch, WINDOW_Y_LENGTH, TJPF_RGB, TJFLAG_FASTDCT) == -1)
+    if (tjDecompress2(_jpegDecompressor, compressed_image, JPEG_SIZE, buffer, WINDOW_X_LENGTH, pitch, WINDOW_Y_LENGTH, TJPF_RGB, TJFLAG_FASTDCT) == -1)
         std::cout << "decompressed incorrectly" << std::endl;
-    //free the decompressor.
-    tjDestroy(_jpegDecompressor);
 }
 
 int main(){
-    int pitch = 1920*3;
-    uint32_t offset;
-    //create a buffer for the message struct (declared in loop)
-    unsigned char* temp_compressed_message_buffer = new unsigned char[CAMERA_FRAME_BUFFER_SIZE];
-    //array of buffers to hold the compressed messages
     BufferItem* frame_buffer = new BufferItem[CAMERA_FRAME_BUFFER_COUNT];
-    //pointer to where we keep the pixel data
-    unsigned char* pixel_location;
+    /*
+        This creates as many buffers as we specified earlier.
+        These buffers will hold the compressed JPEG data.
+     */
     for(int i = 0; i < CAMERA_FRAME_BUFFER_COUNT; i++){
         frame_buffer[i].data_sections = new unsigned char[CAMERA_FRAME_BUFFER_SIZE];
     }
+    /*
+        We need a single additional buffer to store
+        the data we recieve from the network.
+     */
+    unsigned char* temp_compressed_message_buffer = new unsigned char[CAMERA_FRAME_BUFFER_SIZE];
+    /*
+        We have to write the pixel data to a specific location,
+        so we need a pointer to store that location
+     */
+    unsigned char* pixel_location;
+    /*
+        Pitch is the number of bytes in a row of an image.
+        (This value can change if the hardware uses padding)
+     */
+    int pitch = 1920*3;
+    uint32_t offset;
     uint8_t indx = 0;
     network::Connection conn;
     const char* destination_port = "127.0.0.1";
-    //allow error to fade out of scope
+    //This will allow error to fade out of scope after we no longer need it.
     {
         network::Error error = network::connect(&conn, destination_port, 45546, 45545) ;
         if(error == network::Error::OPEN_SOCKET)
-            std::cout << "Open socket error: " << std::endl;
+            std::cout << "Error opening the socket." << std::endl;
         else if(error == network::Error::BIND_SOCKET)
-            std::cout << "Binding socket error" << std::endl;
+            std::cout << "Error binding the socket" << std::endl;
     }
-    //check for failure
+    /*
+        Here we ckeck if any of the SDL components are
+        created incorrectly. If they fail we return from main
+        because we cannot display anything.
+     */
     if (SDL_Init(SDL_INIT_VIDEO) != 0){
         std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return 1;
     }
-    //creates window. pramaeters are window title, x start pos, y start pos,
-    //x end pos, and y end pos. We use the dimensions of the window provided above
-    SDL_Window *win = SDL_CreateWindow("Camera feed!", 0, 0, WINDOW_X_LENGTH, WINDOW_Y_LENGTH, SDL_WINDOW_SHOWN);
-    //check for failure
+    /*
+        This creates an SDL window. The parameters are the window title,
+        x start pos and y start pos (both as an offset from the top
+        left corner), x end pos, and y end pos. We use the dimensions
+        of the window provided above
+     */
+    SDL_Window *win = SDL_CreateWindow("Camera feed!", 0, 0,
+        WINDOW_X_LENGTH, WINDOW_Y_LENGTH, SDL_WINDOW_SHOWN);
     if (win == nullptr){
         std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return 1;
     }
     /*
-        makes a renderer. args are the window to render to, the index of the 
-        rendering driver, and the rendering flags or'd together
+        This makes a renderer. The arguments are
+        the window to render to, the index of the rendering driver,
+        and the rendering flags or'd together
         SDL ACCELERATED uses hardware acceleration, and SDL PRESENTVSYNC
-        is present is synchronized with the refresh rate
-        Other flags are TARGETTEXTURE -- supports redering to texture
-        and SOFTWARE -- which means the renderer is a software fallback 
+        synchronizes with the refresh rate
+        Other flags are TARGETTEXTURE which supports redering to texture
+        and SOFTWARE which means the renderer has a software fallback 
         should the hardware accelerated rendering fail
     */
     SDL_Renderer *ren = SDL_CreateRenderer(win,-1,
@@ -86,11 +120,19 @@ int main(){
         SDL_Quit();
         return 1;
     }
+     /*
+        This makes a texture. The arguments are
+        the renderer, the pixel storage format (3 bytes in our case)
+        and the rendering flags or'd together
+        here we only use TEXTUREACCESS_STREAMING, which allows for 
+        the texture to change repeatedly.
+    */
     SDL_Texture *texture = SDL_CreateTexture(ren, SDL_PIXELFORMAT_RGB24, 
         SDL_TEXTUREACCESS_STREAMING, 1920, 1080);
     bool running = true;
     SDL_Event event;
-    do{
+    while(running){
+        //If someone clicks the x, exit the program.
         if (SDL_PollEvent(&event)) {
             switch (event.type) {
                 case SDL_QUIT:
@@ -98,49 +140,70 @@ int main(){
                     break;
             }
         }
+        //This is the standard network protocall to recieve a packet.
         network::poll_incoming(&conn);
         network::Message message;
         while(network::dequeue_incoming(&conn, &message)) {
-            //std::cout << "dequeueing messages" << std::endl;
             switch (message.type) {
                 case network::MessageType::CAMERA: {
+                    //We need a CameraMessage to hold the packet data.
                     network::CameraMessage frame{};
+                    /*
+                        Here we point the new message data
+                        to our temporary buffer.
+                     */
                     frame.data = temp_compressed_message_buffer;
                     network::deserialize(message.buffer, &frame);
-                    //std::cout << "deserialized message, Size: " << frame.size << std::endl;
-                    //std::cout << "deserialized message index: " << frame.frame_index << std::endl;
+                    /*
+                        Since we know that camera packets will only be
+                        sent out a single time we can manage the buffer
+                        on a modular system. Therefore we get the index
+                        by taking the frame index mod the total number
+                        of buffers.
+                     */
                     indx = frame.frame_index % CAMERA_FRAME_BUFFER_COUNT;
+                    /*
+                        If we have done a full loop and there is a new
+                        frame index, then we overwrite the old data.
+                     */
                     if(frame_buffer[indx].frame_index != frame.frame_index){
                         frame_buffer[indx].frame_index = frame.frame_index;
-                        //std::cout << "Section count: " << unsigned(frame.section_count) << std::endl;
-                        //printf(%" PRIu16 "\n", frame_buffer[indx].sections_remaining);
                         frame_buffer[indx].frame_index = frame.frame_index;
                         frame_buffer[indx].sections_remaining = frame.section_count;
                         frame_buffer[indx].data_size = 0;
-                        //std::cout << "Initial Sections Remaining: " << unsigned(frame_buffer[indx].sections_remaining) << std::endl;
                     }
                     frame_buffer[indx].data_size += frame.size;
                     frame_buffer[indx].sections_remaining--;
-                    //copy the frame data to our buffer (offset by the section index)
+                    /*
+                        This section handles any data that comes out of
+                        order. Each data section is written at the size
+                        of a signle data packet * the section index.
+                     */
                     offset = (CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE * frame.section_index);
                     std::memcpy(frame_buffer[indx].data_sections + offset , frame.data, frame.size);
-                    //std::cout << "Remaining sections: " << unsigned(frame_buffer[indx].sections_remaining) << std::endl;
                     
                     if(frame_buffer[indx].sections_remaining  == 0){
                         /*
-                            if there are no sections remaining,
-                            lock the pixels for writing, pass the location into
-                            decompress where the pixel data is written.
-                            then unlock the texture again to render
+                            If there are no sections remaining,
+                            lock the pixels for writing, and pass the
+                            location into decompress. Decompress
+                            writes the decompressed pixel data to 
+                            pixel_location. Then we unlock the texture
+                            again to render it to the screen.
                          */
                         SDL_LockTexture(texture, NULL, (void **) &pixel_location, &pitch);
                         decompress(pixel_location , frame_buffer[indx].data_sections, pitch);
                         SDL_UnlockTexture(texture);
-                        //get rid of any garbage that may be in renderer
+                        //This clears anything currently in the renderer
                         SDL_RenderClear(ren);
-                        //load the texture onto the renderer. NULL renders to the full extent of the window
+                        /*
+                            This loads the texture onto the renderer.
+                            The NULL parameters specify the demensions
+                            of the window to render to. Null renders to 
+                            the full extent of the window.
+                         */
                         SDL_RenderCopy(ren, texture, NULL, NULL);
-                        //render the contents of the renderer
+                        //This renders the contents of the renderer.
                         SDL_RenderPresent(ren);
                     }
                     break;
@@ -150,8 +213,12 @@ int main(){
             }
             network::return_incoming_buffer(message.buffer);
         }
-    }while(running);
-    //free all the SDL parts to avoid memory leaks
+    }
+    /*
+        We need to free the decompressor and
+        SDL components to avoid memory leaks
+     */
+    tjDestroy(_jpegDecompressor);
     SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
