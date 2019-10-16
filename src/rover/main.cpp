e1f029b0bc070f8abadcc7fe7d24409379a21087
AlexanderRBurke <Alexburkeburkeburke@gmail.com>
2019-10-16 11:32:09 -0400

    Added dynamic camera additions, still not done but need to commit so I can merge the camera matrix in

--- src/rover/main.cpp
+++ src/rover/main.cpp
@@ -219,60 +219,98 @@ int main()
 	uint8_t* jpeg_buffer = (uint8_t*) malloc(jpeg_size);
 
     while (true) {
-        for (size_t i = 0; i < streams.size(); i++) {
-            camera::CaptureSession *cs = streams[i];
+		// Try to open any new camera streams.
+		for (int i = streams.size(); i < MAX_STREAMS; i++) {
+			camera::CaptureSession *cs = new camera::CaptureSession;
 
-            // Grab a frame.
-            uint8_t *frame_buffer;
-            size_t frame_size;
-            {
-                camera::Error err = camera::grab_frame(cs, &frame_buffer, &frame_size);
-                if (err != camera::Error::OK) {
-                    std::cout << "Camera 0: " << camera::get_error_string(err) << std::endl;
-                }
-            }
+			char name_filename_buffer[100];
+			sprintf(name_filename_buffer, "/sys/class/video4linux/video%d/name", i);
 
-            // Decode the frame and encode it again to set our desired quality.
-            static uint8_t raw_buffer[CAMERA_WIDTH * CAMERA_HEIGHT * 3];
+			FILE* name_file = fopen(name_filename_buffer, "r");
+			if (!name_file) continue;
 
-            // Decompress into a raw frame.
-            tjDecompress2(decompressor, frame_buffer, frame_size, raw_buffer, CAMERA_WIDTH, 3 * CAMERA_WIDTH,
-                          CAMERA_HEIGHT, TJPF_RGB, 0);
+			fscanf(name_file, "%s\n", name_filename_buffer);
+			printf("> Found camera with name %s\n", name_filename_buffer);
 
-            // Recompress into jpeg buffer.
-            tjCompress2(compressor, raw_buffer, CAMERA_WIDTH, 3 * CAMERA_WIDTH, CAMERA_HEIGHT, TJPF_RGB, &frame_buffer,
-                        &frame_size, TJSAMP_420, 30, TJFLAG_NOREALLOC);
+			fclose(name_file);
 
-            /*
-                    2. Send out packets
-                            - Create camera messages and send Camera packets
-            */
+			if (strcmp("ZED", name_filename_buffer) == 0) continue;
 
-            // Send the frame.
-            // Calculate how many buffers we will need to send the entire frame
-            uint8_t num_buffers = (frame_size / CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE) + 1;
+			char filename_buffer[13]; // "/dev/video" is 10 chars long, leave 2 for numbers, and one for null terminator.
+			sprintf(filename_buffer, "/dev/video%d", i);
 
-            for (uint8_t j = 0; j < num_buffers; j++) {
-                // This accounts for the last buffer that is not completely divisible
-                // by the defined buffer size, by using up the remaining space calculated
-                // with modulus    -yu
-                uint16_t buffer_size = (j != num_buffers - 1) ? CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE
-                                                              : frame_size % CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE;
+			camera::Error err = camera::open(cs, filename_buffer, CAMERA_WIDTH, CAMERA_HEIGHT);
+			if (err != camera::Error::OK) {
+				delete cs;
+				break;
+			}
 
-                network::CameraMessage message = {
-                    static_cast<uint8_t>(i),                                // stream_index
-                    static_cast<uint16_t>(frame_counter),                   // frame_index
-                    static_cast<uint8_t>(j),                                // section_index
-                    num_buffers,                                            // section_count
-                    buffer_size,                                            // size
-                    frame_buffer + (CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE * j) // data
-                };
+			// Start the camera.
+			err = camera::start(cs);
+			if (err != camera::Error::OK) {
+				camera::close(cs);
+				delete cs;
+				continue;
+			}
 
-                network::publish(&r_feed, &message);
-            }
+			// It was opened and started. Add it to the list of streams.
+			streams.push_back(cs);
+		}
+		
+		for (size_t i = 0; i < streams.size(); i++) {
+			camera::CaptureSession *cs = streams[i];
 
-            camera::return_buffer(cs);
-        }
+			// Grab a frame.
+			uint8_t *frame_buffer;
+			size_t frame_size;
+			{
+				camera::Error err = camera::grab_frame(cs, &frame_buffer, &frame_size);
+				if (err != camera::Error::OK) {
+					std::cout << "Camera 0: " << camera::get_error_string(err) << std::endl;
+				}
+			}
+
+			// Decode the frame and encode it again to set our desired quality.
+			static uint8_t raw_buffer[CAMERA_WIDTH * CAMERA_HEIGHT * 3];
+
+			// Decompress into a raw frame.
+			tjDecompress2(decompressor, frame_buffer, frame_size, raw_buffer, CAMERA_WIDTH, 3 * CAMERA_WIDTH,
+					CAMERA_HEIGHT, TJPF_RGB, 0);
+
+			// Recompress into jpeg buffer.
+			tjCompress2(compressor, raw_buffer, CAMERA_WIDTH, 3 * CAMERA_WIDTH, CAMERA_HEIGHT, TJPF_RGB, &frame_buffer,
+					&frame_size, TJSAMP_420, 30, TJFLAG_NOREALLOC);
+
+			/*
+			   2. Send out packets
+			   - Create camera messages and send Camera packets
+			 */
+
+			// Send the frame.
+			// Calculate how many buffers we will need to send the entire frame
+			uint8_t num_buffers = (frame_size / CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE) + 1;
+
+			for (uint8_t j = 0; j < num_buffers; j++) {
+				// This accounts for the last buffer that is not completely divisible
+				// by the defined buffer size, by using up the remaining space calculated
+				// with modulus    -yu
+				uint16_t buffer_size = (j != num_buffers - 1) ? CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE
+					: frame_size % CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE;
+
+				network::CameraMessage message = {
+					static_cast<uint8_t>(i),                                // stream_index
+					static_cast<uint16_t>(frame_counter),                   // frame_index
+					static_cast<uint8_t>(j),                                // section_index
+					num_buffers,                                            // section_count
+					buffer_size,                                            // size
+					frame_buffer + (CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE * j) // data
+				};
+
+				network::publish(&r_feed, &message);
+			}
+
+			camera::return_buffer(cs);
+		}
 
 		// LIDAR stuff.
 		if (lidar_send_timer.ready()) {
@@ -286,61 +324,61 @@ int main()
 				message.points[i] = (uint16_t) lidar_points[i];
 			}
 
-            network::publish(&r_feed, &message);
+			network::publish(&r_feed, &message);
 
 			for (auto point : lidar_points) {
 				int64_t point_enc = point;
 			}
 		}
 
-        unsigned char* zed_image;
-        int zed_stride;
-        zed::Pose zed_pose;
-        if (zed::grab(&zed_image, &zed_stride, &zed_pose) == zed::Error::OK) {
+		unsigned char* zed_image;
+		int zed_stride;
+		zed::Pose zed_pose;
+		if (zed::grab(&zed_image, &zed_stride, &zed_pose) == zed::Error::OK) {
 			jpeg_size = tjBufSize(1280, 720, TJSAMP_422);
 
 			auto tj_err = tjCompress2(compressor, zed_image, 1280, 
-									zed_stride, 720, TJPF_BGRA, 
-									(unsigned char**)&jpeg_buffer, &jpeg_size, TJSAMP_422, 40, TJFLAG_NOREALLOC);
+					zed_stride, 720, TJPF_BGRA, 
+					(unsigned char**)&jpeg_buffer, &jpeg_size, TJSAMP_422, 40, TJFLAG_NOREALLOC);
 
-/*
-			auto tj_err = tjCompressFromYUV(compressor, zed_image.getPtr<unsigned char>(), 1280,
-											zed_image.getStepBytes(), 720, TJSAMP_444, &jpeg_buffer,
-											&jpeg_size, 40, TJFLAG_NOREALLOC);
-*/
+			/*
+			   auto tj_err = tjCompressFromYUV(compressor, zed_image.getPtr<unsigned char>(), 1280,
+			   zed_image.getStepBytes(), 720, TJSAMP_444, &jpeg_buffer,
+			   &jpeg_size, 40, TJFLAG_NOREALLOC);
+			 */
 			if (tj_err != 0) {
 				fprintf(stderr, "[!] tjCompress failed: %s\n", tjGetErrorStr2(compressor));
 			}
 
-            // Send the frame.
-            // Calculate how many buffers we will need to send the entire frame
-            uint8_t num_buffers = (jpeg_size / CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE) + 1;
+			// Send the frame.
+			// Calculate how many buffers we will need to send the entire frame
+			uint8_t num_buffers = (jpeg_size / CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE) + 1;
 
-            for (uint8_t j = 0; j < num_buffers; j++) {
-                // This accounts for the last buffer that is not completely divisible
-                // by the defined buffer size, by using up the remaining space calculated
-                // with modulus    -yu
-                uint16_t buffer_size = (j != num_buffers - 1) ? CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE
-                                                              : jpeg_size % CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE;
+			for (uint8_t j = 0; j < num_buffers; j++) {
+				// This accounts for the last buffer that is not completely divisible
+				// by the defined buffer size, by using up the remaining space calculated
+				// with modulus    -yu
+				uint16_t buffer_size = (j != num_buffers - 1) ? CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE
+					: jpeg_size % CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE;
 
-                network::CameraMessage message = {
-                    static_cast<uint8_t>(streams.size()),                                // stream_index
-                    static_cast<uint16_t>(frame_counter),                   // frame_index
-                    static_cast<uint8_t>(j),                                // section_index
-                    num_buffers,                                            // section_count
-                    buffer_size,                                            // size
-                    jpeg_buffer + (CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE * j) // data
-                };
+				network::CameraMessage message = {
+					static_cast<uint8_t>(streams.size()),                                // stream_index
+					static_cast<uint16_t>(frame_counter),                   // frame_index
+					static_cast<uint8_t>(j),                                // section_index
+					num_buffers,                                            // section_count
+					buffer_size,                                            // size
+					jpeg_buffer + (CAMERA_MESSAGE_FRAME_DATA_MAX_SIZE * j) // data
+				};
 
-                network::publish(&r_feed, &message);
-            }
+				network::publish(&r_feed, &message);
+			}
 
-            if (zed_timer.ready()) {
-                // TODO: SEND NETWORK UPDATES AND UPDATE AUTONOMY.
-            }
+			if (zed_timer.ready()) {
+				// TODO: SEND NETWORK UPDATES AND UPDATE AUTONOMY.
+			}
 		}
-        // Increment global (across all streams) frame counter. Should be ok. Should...
-        frame_counter++;
+		// Increment global (across all streams) frame counter. Should be ok. Should...
+		frame_counter++;
 
 #if 0
 		// IMU stuff.
@@ -350,47 +388,47 @@ int main()
 		}
 #endif
 
-        // Receive incoming messages
-        network::IncomingMessage message;
-        while (true) {
+		// Receive incoming messages
+		network::IncomingMessage message;
+		while (true) {
 			auto neterr = network::receive(&r_feed, &message);
 			if (neterr != network::Error::OK) {
 				if (neterr == network::Error::NOMORE) {
 					break;
 				}
 
-                printf("[!] Network error on receive!\n");
+				printf("[!] Network error on receive!\n");
 
 				break;
 			}
 
-            switch (message.type) {
-                case network::MessageType::MOVEMENT: {
-                    network::MovementMessage movement;
-                    network::deserialize(&message.buffer, &movement);
+			switch (message.type) {
+				case network::MessageType::MOVEMENT: {
+														 network::MovementMessage movement;
+														 network::deserialize(&message.buffer, &movement);
 
-					// printf("Got movement with %d, %d\n", movement.left, movement.right);
+														 // printf("Got movement with %d, %d\n", movement.left, movement.right);
 
-					suspension::Direction left_direction = movement.left < 0 ? suspension::BACKWARD : suspension::FORWARD;
-					suspension::Direction right_direction = movement.right < 0 ? suspension::BACKWARD : suspension::FORWARD; 
+														 suspension::Direction left_direction = movement.left < 0 ? suspension::BACKWARD : suspension::FORWARD;
+														 suspension::Direction right_direction = movement.right < 0 ? suspension::BACKWARD : suspension::FORWARD; 
 
-					uint8_t left_speed = movement.left < 0 ? (uint8_t) ((-movement.left)) : (uint8_t) (movement.left);
-					uint8_t right_speed = movement.right < 0 ? (uint8_t) ((-movement.right)) : (uint8_t) (movement.right);
+														 uint8_t left_speed = movement.left < 0 ? (uint8_t) ((-movement.left)) : (uint8_t) (movement.left);
+														 uint8_t right_speed = movement.right < 0 ? (uint8_t) ((-movement.right)) : (uint8_t) (movement.right);
 
-					if (suspension_update_timer.ready()) {
-						suspension::update(suspension::LEFT, left_direction, left_speed);
-						suspension::update(suspension::RIGHT, right_direction, right_speed);
-					}
+														 if (suspension_update_timer.ready()) {
+															 suspension::update(suspension::LEFT, left_direction, left_speed);
+															 suspension::update(suspension::RIGHT, right_direction, right_speed);
+														 }
 
-                    break;
-                }
-                default:
-                    break;
-            }
-        }
+														 break;
+													 }
+				default:
+													 break;
+			}
+		}
 
-        // Update feed statuses.
-        network::update_status(&r_feed);
-        network::update_status(&bs_feed);
-    }
+		// Update feed statuses.
+		network::update_status(&r_feed);
+		network::update_status(&bs_feed);
+	}
 }
