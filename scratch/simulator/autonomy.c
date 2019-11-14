f5071ed641af7e8b76938da53805b8aafd6ecb84
Michael Schutzman <michaelschutzman@gmail.com>
2019-11-13 22:48:01 -0500

    outputting densities. mag always 0

--- scratch/simulator/autonomy.c
+++ scratch/simulator/autonomy.c
@@ -17,10 +17,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define PI 3.14159265
 const int ws = 10;
 const float B = 2;
-const int alpha = 1;
+const int alpha = 5;
 const int l = 5;
 const float cell_size = 1;
 const float threshold = 0.3;
@@ -29,30 +31,41 @@ typedef struct {
     int start, end;
 } Valley;
 
-void get_polar_obstacle_densities(World world, float rover_x, float rover_y, float rover_angle, float* h, int n, float A) {
+void get_polar_obstacle_densities(World world, float rover_x, float rover_y, float rover_angle, float h[], int n, float A) {
     int c_x, c_y;
     float world_x, world_y;
     float cell_magnitude, cell_direction;
     world_to_cell(&world, rover_x, rover_y, &c_x, &c_y);
+    printf("roverx: %f\n", rover_x);
+    printf("rovery: %f\n", rover_y);
+    printf("cx: %d\n", c_x);
+    printf("cy: %d\n", c_y);
     for (int i = (c_y - ws/2); i <= (c_y + ws/2); i++)
     {
         for (int j = (c_x - ws/2); j <= (c_x + ws/2); j++)
         {
             
-            cell_to_world(&world, c_x, c_y, &world_x, &world_y);
-            cell_direction = atan2(world_y - rover_y, world_x - rover_x);
-            printf("cell direction value: %f\n", cell_direction);
-            printf("cx value: %d\n", c_x);
-            printf("cy value: %d\n", c_y);
+            //cell_to_world(&world, c_x, c_y, &world_x, &world_y);
+            cell_to_world(&world, j, i, &world_x, &world_y);
+            cell_direction = atan2(world_y - rover_y, world_x - rover_x) * 180 / PI;
+            cell_direction += 180; // converts from angle -180-180 to angle 0-360
+            printf("cell DIRECTION value: %f\n", cell_direction);
+            printf("j value: %d\n", j);
+            printf("i value: %d\n", i);
             printf("worldx value: %f\n", world_x);
             printf("worldy value: %f\n", world_y);
-            printf("cell mag pt 1: %f\n", pow(occupancy_grid_get(&world.occupancy_grid, c_x, c_y), 2));
-            cell_magnitude = pow(occupancy_grid_get(&world.occupancy_grid, c_x, c_y), 2) * (A - B * pow(pow(rover_y - world_y, 2) + pow(rover_x - world_x, 2), 0.5));
-            int k = abs((int)(cell_direction/alpha));
+            printf("cell mag pt 1: %f\n", pow(occupancy_grid_get(&world.occupancy_grid, j, i), 2));
+            cell_magnitude = pow(occupancy_grid_get(&world.occupancy_grid, j, i), 2) * (A - B * pow(pow(rover_y - world_y, 2) + pow(rover_x - world_x, 2), 0.5));
+            int k = (int)(cell_direction/alpha);
             printf("index value: %d\n", k);
-            printf("cell mag value: %f\n", cell_magnitude);
+            printf("cell MAGNITUDE value: %f\n", cell_magnitude);
             h[k] += cell_magnitude;
         }
+        printf("OUT OF INNER LOOP\n");
+    }
+
+    for (int i = 0; i < 72; i++) {
+        printf("THIS IS THE %d: VALUE: %f\n", i, h[i]);
     }
 
     for (int k = 0; k <= (n-1); k++)
@@ -144,15 +157,22 @@ AutonomyStatus autonomy_step(World* world, float rover_x, float rover_y, float r
     const float dmax = pow(2, 0.5) * (ws - 1) / 2;
     const float A = dmax;
     const int n = 360/alpha;
+    printf("alpha size%u", alpha);
+    printf("n size%d", n);
     float densities[n];
-    printf("size of densities: %zu\n", sizeof(densities));
+    memset(densities, 0, sizeof(float)*n);
+    printf("size of densities: %lu\n", sizeof(densities)/sizeof(densities[0]));
+    for (int i = 0; i < sizeof(densities)/sizeof(densities[0]); i++)
+    {
+        printf("THIS IS THE FIRST FIRST FIRST FIRST PART OF WHAT I'M OUTPUTTING HERE YA GO FOLKS %d: %f\n", i, densities[i]);
+    }
     get_polar_obstacle_densities(*world, rover_x, rover_y, rover_angle, densities, n, A);
     printf ("MADE IT OUT OF FIRST FUNCTION\n");
-    for (int i = 0; i < sizeof(*densities); i++)
+    for (int i = 0; i < sizeof(densities)/sizeof(densities[0]); i++)
     {
-        printf("value %d: %f\n", i, densities[i]);
+        printf("THIS IS THE SECOND PART OF WHAT I'M OUTPUTTING HERE YA GO FOLKS %d: %f\n", i, densities[i]);
     }
-    Valley valleys = get_valleys(densities, n);
+    //Valley valleys = get_valleys(densities, n);
     *out_offset_x = 0.5;
     *out_offset_y = 0.5;
 
