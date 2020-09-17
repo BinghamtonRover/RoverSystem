decaf209d7dee1d12bb8c63fb1b39c59ff3561bd
Daniel Tierney <superdan.tier@gmail.com>
2020-09-17 18:55:35 +0100

    Event handling

--- scratch/accelerated_pi_cam/omx_video_component.cpp
+++ scratch/accelerated_pi_cam/omx_video_component.cpp
@@ -5,12 +5,11 @@ OMXVideoComponent::~OMXVideoComponent() {
 	if (initialized) deinit();
 }
 
-OMX_CALLBACKTYPE OMXVideoComponent::callbacks = { EventHandler: event_handler, FillBufferDone: nullptr };
+OMX_CALLBACKTYPE OMXVideoComponent::callbacks = { EventHandler: event_handler, FillBufferDone: fill_buffer_done };
 
 void OMXVideoComponent::wait_event(VCOS_UNSIGNED events, VCOS_UNSIGNED *retrieved_events) {
-	// 0x1 is an enum for EVENT_ERROR in example, still figuring this out
 	VCOS_UNSIGNED set;
-	vcos_event_flags_get(&flags, events | 0x1, VCOS_OR_CONSUME, VCOS_SUSPEND, &set);
+	vcos_event_flags_get(&flags, events | ComponentEvent::ERROR, VCOS_OR_CONSUME, VCOS_SUSPEND, &set);
 	
 	if (retrieved_events != nullptr) {
 		*retrieved_events = set;
@@ -20,12 +19,21 @@ void OMXVideoComponent::wait_event(VCOS_UNSIGNED events, VCOS_UNSIGNED *retrieve
 OMX_ERRORTYPE OMXVideoComponent::event_handler(OMX_IN OMX_HANDLETYPE comp, OMX_IN OMX_PTR app_data, 
 		OMX_IN OMX_EVENTTYPE event, OMX_IN OMX_U32 data1, OMX_IN OMX_U32 data2, OMX_IN OMX_PTR event_data) {
 	OMXVideoComponent *component = (OMXVideoComponent*) app_data;
-	component->received_event(event, data1, data2);
+	
+	component->received_event(get_event_flags(event, data1));
 	return OMX_ErrorNone;
 }
 
-void OMXVideoComponent::received_event(OMX_IN OMX_EVENTTYPE event, OMX_IN OMX_U32 event_data1, OMX_IN OMX_U32 event_data2) {
-	// TODO: Total implementation needed
+OMX_ERRORTYPE OMXVideoComponent::fill_buffer_done(OMX_IN OMX_HANDLETYPE comp, OMX_IN OMX_PTR app_data, 
+	OMX_IN OMX_BUFFERHEADERTYPE* buffer){
+	OMXVideoComponent *component = (OMXVideoComponent*) app_data;
+	component->received_event(ComponentEvent::FILL_BUFFER_DONE);
+
+	return OMX_ErrorNone;
+}
+
+void OMXVideoComponent::received_event(ComponentEvent event) {
+	vcos_event_flags_set(&flags, event, VCOS_OR);
 }
 
 bool OMXVideoComponent::init() {
@@ -48,8 +56,7 @@ bool OMXVideoComponent::init() {
 		// From the example: I'm not sure why all ports get disabled
 		for (OMX_U32 p = ports.nStartPortNumber; p < ports.nStartPortNumber + ports.nPorts; ++p) {
 			OMX_SendCommand(handle, OMX_CommandPortDisable, p, 0);
-			// 0x4 is EVENT_PORT_DISABLE in example. TODO: enum, existing definition, something other than hex that tells you nothing
-			wait_event(0x4, nullptr);
+			wait_event(ComponentEvent::PORT_DISABLE, nullptr);
 		}
 	}
 	initialized = true;
--- scratch/accelerated_pi_cam/omx_video_component.hpp
+++ scratch/accelerated_pi_cam/omx_video_component.hpp
@@ -1,15 +1,18 @@
 #include <IL/OMX_Broadcom.h>
 #include <interface/vcos/vcos.h>
 
+#include "component_event.hpp"
+
 class OMXVideoComponent {
 private:
 	void wait_event(VCOS_UNSIGNED events, VCOS_UNSIGNED *retrieved_events);
-	void received_event(OMX_IN OMX_EVENTTYPE event, OMX_IN OMX_U32 event_data1, OMX_IN OMX_U32 event_data2);
+	void received_event(ComponentEvent event);
 	
 	bool initialized = false;
 	
 	static OMX_CALLBACKTYPE callbacks;
 public:
+	
 	~OMXVideoComponent();
 	bool init();
 	void deinit();
@@ -19,8 +22,13 @@ public:
 		OMX_IN OMX_EVENTTYPE event, 
 		OMX_IN OMX_U32 data1, 
 		OMX_IN OMX_U32 data2, 
-		OMX_IN OMX_PTR event_data);
-	
+		OMX_IN OMX_PTR event_data
+	);
+	static OMX_ERRORTYPE fill_buffer_done(
+		OMX_IN OMX_HANDLETYPE comp, 
+		OMX_IN OMX_PTR app_data, 
+		OMX_IN OMX_BUFFERHEADERTYPE* buffer
+	);
 	// TODO: Better encapsulation for these types
 	OMX_STRING name;
 	OMX_HANDLETYPE handle;
