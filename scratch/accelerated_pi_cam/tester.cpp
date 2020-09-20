f61bc3159cf73ef200e4cd9bdb4acdd75e2a6d81
Daniel Tierney <superdan.tier@gmail.com>
2020-09-20 20:56:13 +0100

    Comments and C++ style IO

--- scratch/accelerated_pi_cam/tester.cpp
+++ scratch/accelerated_pi_cam/tester.cpp
@@ -1,32 +1,42 @@
 #include <iostream>
-#include <time.h>
-#include <fcntl.h>
+#include <fstream>
+#include <chrono>
+
 #include "video_encoder.hpp"
 #include "video_system_exception.hpp"
 
+/*
+	This example records 3 seconds of encoded video and outputs to a file
+	The file needs a container before you can play it. Install MKVToolNix and use this command:
+		mkvmerge --default-duration 0:30p -o video.mkv video.h264
+ */
+
 int main() {
-	int fd = open("video.h264", O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0666);
-	if (fd == -1) return -1;
+	std::ofstream video_out_file;
+	video_out_file.open("video.h264");
+	
 	try {
-		VideoEncoder v;
-		v.init();
-		v.start_video();
-		struct timespec spec;
-		clock_gettime(CLOCK_MONOTONIC, &spec);
-		long now = spec.tv_sec*1000 + spec.tv_nsec / 1.0e6;
-		long end = now + 3000;
 		
+		VideoEncoder video_system;
+		video_system.init();	// Load drivers, connect camera and encoder together
+		video_system.start_video();	// Activate the video components (does not output anything)
+	
+		// Keep recording frames for 3 seconds
+		std::chrono::time_point<std::chrono::system_clock> end_time = std::chrono::system_clock::now() + std::chrono::seconds(3);
 		for (;;) {
-			OMX_BUFFERHEADERTYPE *frame = v.get_frame();
-			pwrite(fd, frame->pBuffer, frame->nFilledLen, frame->nOffset);
-			clock_gettime(CLOCK_MONOTONIC, &spec);
-			if (spec.tv_sec*1000 + spec.tv_nsec/1.0e6 >= end) break;
+			OMX_BUFFERHEADERTYPE *frame = video_system.get_frame();	// Encode the latest frame AND get the buffer
+			
+			video_out_file.write( (char*)frame->pBuffer, frame->nFilledLen );
+			
+			if (std::chrono::system_clock::now() >= end_time) break;
 		}
-		close(fd);
+		video_out_file.close();
 		return 0;
+		
 	} catch (const VideoSystemException& e) {
 		std::cout << "Caught an exception: " << e.get_details() << std::endl;
-		close(fd);
+		video_out_file.close();
 		return 1;
 	}
+	
 }
