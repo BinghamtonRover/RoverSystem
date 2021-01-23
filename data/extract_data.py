016c68783c3b04c1a5ca8260762ee55993959e30
Kurt Manrique-Nino <kurt.manrique.n@gmail.com>
2021-01-23 13:26:11 -0500

    clear canvas after drawing plot and added some comments

--- data/extract_data.py
+++ data/extract_data.py
@@ -10,7 +10,7 @@ temp_level = []
 hum_level = []
 meth_level = []
 
-
+#Open .csv file
 with open('practice_data.csv', encoding='utf-8-sig') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
@@ -20,8 +20,9 @@ with open('practice_data.csv', encoding='utf-8-sig') as csvfile:
         temp_level.append(row['Temp'])
         hum_level.append(row['Humidity'])
         meth_level.append(row['Methane'])
+csvfile.close()
 
-
+#Remove forward slashes put by Excel/Calc
 for char in timestamp[0]:
     if char == "/":
         temp = timestamp[0].replace("/", "-")
@@ -33,6 +34,7 @@ plt.ylabel("PH Level")
 plt.title("PH Levels")
 plt.xticks(size=6)
 plt.savefig('Science_PH_' + temp + '.png')
+plt.clf()
 
 
 #Making a bar graph for CO2
@@ -42,6 +44,7 @@ plt.ylabel("CO2 Level (PPM)")
 plt.title("CO2 Levels")
 plt.xticks(size=6)
 plt.savefig('Science_CO2 ' + temp + '.png')
+plt.clf()
 
 #Making a bar graph for Temp
 plt.bar(timestamp, [int(x) for x in temp_level], color='g')
@@ -50,6 +53,7 @@ plt.ylabel("Temperature (Celsius)")
 plt.title("Temperature")
 plt.xticks(size=6)
 plt.savefig('Science_Temp ' + temp + '.png')
+plt.clf()
 
 #Making a bar graph for Humidity
 plt.bar(timestamp, hum_level, color='g')
@@ -58,6 +62,7 @@ plt.ylabel("Humidity (%)")
 plt.title("Humidity")
 plt.xticks(size=6)
 plt.savefig('Science_Humidity ' + temp + '.png')
+plt.clf()
 
 #Making a bar graph for Mehtane
 plt.bar(timestamp, [int(x) for x in meth_level], color='g')
@@ -66,3 +71,4 @@ plt.ylabel("Methane (%)")
 plt.title("Methane")
 plt.xticks(size=6)
 plt.savefig('Science_Meth ' + temp + '.png')
+plt.clf()
